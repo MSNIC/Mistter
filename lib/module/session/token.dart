de8039e63879cfbf63f616f3d3a99aefa55fb619
49372s <kyouyouidsiyou@gmail.com>
2024-11-28 21:42:32 +0900

    fix: Flutter Analytics...Clear messages

--- lib/module/session/token.dart
+++ lib/module/session/token.dart
@@ -2,7 +2,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 Future<bool>checkLoginToken() async {
   var spH = await SharedPreferences.getInstance();
-  var token = await spH.getString("active_token");
+  var token = spH.getString("active_token");
   if(token == "" || token == null){
     return false;
   }else{
--- lib/ui/login/login.dart
+++ lib/ui/login/login.dart
@@ -1,17 +1,14 @@
-import 'dart:ui';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:uuid/uuid.dart';
 
 Widget loginDisplay(){
-  final TextEditingController _controller = TextEditingController();
-  final TextEditingController _password = TextEditingController();
-  var url;
-  var permits;
+  final TextEditingController controller = TextEditingController();
+  final TextEditingController password = TextEditingController();
+  Uri url;
+  String permits;
   return Column(
     mainAxisAlignment: MainAxisAlignment.center,
     children: [
@@ -22,7 +19,7 @@ Widget loginDisplay(){
               fontSize: 28,
               fontWeight: FontWeight.w800
             ),),
-            TextField(controller: _controller,
+            TextField(controller: controller,
               decoration: const InputDecoration(
                 labelText: 'サーバーアドレス',
                 prefixIcon: Icon(Icons.dns_outlined),
@@ -32,18 +29,18 @@ Widget loginDisplay(){
               textInputAction: TextInputAction.next,
 
             ),
-            TextField(controller: _password,
+            TextField(controller: password,
                 obscureText: false,
                 decoration: InputDecoration(
                   labelText: 'アカウントID',
                   prefixIcon: const Icon(Icons.perm_identity_outlined), prefixText: '@',
                   counter: Builder(builder: (BuildContext context) {
                    return ElevatedButton(onPressed: ()=>{
-                      if(_controller.text != "" && _password.text != ""){
+                      if(controller.text != "" && password.text != ""){
                         Fluttertoast.showToast(
-                            msg: "${_controller.text}へ移動します"),
+                            msg: "${controller.text}へ移動します"),
                         permits = "read:account,write:notes,read:blocks,write:blocks,read:drive,write:drive,read:favorites,write:favorites,read:following,write:following,read:messaging,write:messaging,read:mutes,write:mutes,write:account,read:notifications,write:notifications,read:reactions,write:reactions,write:votes,read:pages,write:pages,write:page-likes,read:page-likes,read:user-groups,write:user-groups,read:channels,write:channels,write:report-abuse",
-                        url = Uri.parse('https://${_controller.text}/miauth/${const Uuid().v4()}?name=Mistter&permission=$permits'),
+                        url = Uri.parse('https://${controller.text}/miauth/${const Uuid().v4()}?name=Mistter&permission=$permits'),
                         launchUrl(url)
                             //Navigator.push(context,MaterialPageRoute(builder: (context)=>loginPageMiAuth(_controller.text, _password.text)))
                       }else{
