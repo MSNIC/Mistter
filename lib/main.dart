2ccf9b92cf2e8794a3bf70457a9705f583b61e01
49372s <kyouyouidsiyou@gmail.com>
2024-11-28 21:41:52 +0900

    fix: Flutter Analytics...Clear caution

--- lib/main.dart
+++ lib/main.dart
@@ -46,6 +46,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   void initState() {
+    super.initState();
     // TODO: implement initState
     _isLoginSuccessed = checkLoginToken();
   }
